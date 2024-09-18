9c1feaf89f7274c4d057aec173b06ea6b9a34915
pratikanand13 <93212319+pratikanand13@users.noreply.github.com>
2024-09-18 14:08:22 +0530

    Delete src/db/mongoose.js

--- src/db/mongoose.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-// Connect to MongoDB using the connection string from the environment variable
-mongoose.connect("mongodb+srv://pratikzx:2cHI4yJvyPwyWsKn@babadb.4w2vy.mongodb.net/?retryWrites=true&w=majority&appName=BabaDB")
-.then(() => console.log("Connected to MongoDB"))
-.catch(err => console.error("MongoDB connection error:", err));
